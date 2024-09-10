44de7f287dc635e363743bbbf1fb794d1a5bbd51
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-10 11:58:47 +0600

    splash screen done

---
+++ lib/pages/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'LoginPage/login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    goToLogin();
+  }
+
+  void goToLogin() {
+    Future.delayed(const Duration(seconds: 2)).then((value) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+          (route) => false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.cyan,
+      body: Center(child: Icon(Icons.ac_unit_outlined,size: 50,),)
+    );
+  }
+}
