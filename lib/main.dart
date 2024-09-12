3d25a476b9f433e97a42474867ebfdbd6063ef44
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-12 15:04:05 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:caretutors/routes/app_route_pages.dart';
 import 'package:caretutors/utils/color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final documentDir = await getApplicationDocumentsDirectory();
+  Hive.init(documentDir.path);
   runApp(const MyApp());
 }
 
